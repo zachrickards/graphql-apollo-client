ee3073e017960069adf6999a1b0c331cbf00a8ff
Zachary Rickards <zach.rickards@gmail.com>
2017-11-28 14:16:53 -0500

    Song list component and adding the create song

---
+++ client/components/SongCreate.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class SongCreate extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Create a New Song</h3>
+        <form>
+          <label>Song Title:</label>
+          <input />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SongCreate;
--- server/server.js
+++ server/server.js
@@ -8,16 +8,13 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://lyrical:lyrical@ds115866.mlab.com:15866/zrlyricaldb';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
-}
+} 
 
 mongoose.Promise = global.Promise;
-mongoose.connect(MONGO_URI);
-mongoose.connection
-    .once('open', () => console.log('Connected to MongoLab instance.'))
-    .on('error', error => console.log('Error connecting to MongoLab:', error));
+mongoose.connect(MONGO_URI,  { useMongoClient: true });
 
 app.use(bodyParser.json());
 app.use('/graphql', expressGraphQL({
